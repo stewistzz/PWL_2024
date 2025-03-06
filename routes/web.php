2c69af5b64312baf8c5cdfd1abb5c1ef75aeca06
Dimas Nugroho <dimassetyonugroho30@gmail.com>
2025-03-06 10:39:42 +0700

    add route /hello

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// praktikum 1 - langkah 2
+Route::get('/hello', function () {
+    return "Hello World";
+});
